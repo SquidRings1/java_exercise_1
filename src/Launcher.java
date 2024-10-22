caf6c9519121be977e65aba8ea6e3494d14a1717
SquidRings1 <146839825+SquidRings1@users.noreply.github.com>
2024-10-22 17:07:31 +0200

    remove loop and create hello message

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        
         var scanner = new Scanner(System.in);
         String entry = scanner.nextLine();
 
-        for (;;) {
-            if (entry.equals("exit")) {
-                System.exit(0);
-            } else {
-                System.out.println("Unknown command");
-            }
+        if (entry.equals("exit")) {
+            System.exit(0);
+        } else {
+            System.out.println("Unknown command");
         }
     }
 }
