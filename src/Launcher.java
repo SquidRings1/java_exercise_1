fde2ac85a928aa8fff5ceed2dd6c4ae3661c8a23
SquidRings1 <146839825+SquidRings1@users.noreply.github.com>
2024-10-22 17:11:21 +0200

    fix the quit message

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,11 +4,11 @@ public class Launcher {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        
+
         var scanner = new Scanner(System.in);
         String entry = scanner.nextLine();
 
-        if (entry.equals("exit")) {
+        if (entry.equals("quit")) {
             System.exit(0);
         } else {
             System.out.println("Unknown command");
