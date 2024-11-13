cd0b5af413e5ba13b9aae70f6b8e5da607c6b63d
SquidRings1 <146839825+SquidRings1@users.noreply.github.com>
2024-11-13 11:15:24 +0100

    fix warnings for scanner

--- src/Launcher.java
+++ src/Launcher.java
@@ -10,12 +10,16 @@ public class Launcher {
         for (;;) {
             String entry = scanner.nextLine();
             if (entry.equals("quit")) {
+                scanner.close();
                 break;
             } else if (entry.equals("fibo")) {
                 System.out.println("Enter a number");
                 int n = scanner.nextInt();
                 Fibonacci fib = new Fibonacci(n);
                 System.out.println(fib.execute_fibonacci(n));
+                break;
+            } else if (entry.equals("freq")) {
+                System.out.println("not implemented yet");
                 break;
             } else {
                 System.out.println("Unknown command");
